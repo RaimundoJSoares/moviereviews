fb622c2846c64194e5a68f069fd5edc8f549b361
RaimundoJSoares <raianimes1000@gmail.com>
2023-01-14 14:42:48 -0400

    migrations

--- movie/views.py
+++ movie/views.py
@@ -5,9 +5,9 @@ def home(request):
   searchTerm = request.GET.get('searchMovie')
   return render (request, 'home.html', {'searchTerm': searchTerm})
 
-def about(request): 
-  return HttpResponse('<h1> This is a page ABOUT</h1>')
-
 def signup(request):
   email = request.GET.get('email')
   return render(request, 'signup.html', {'email': email})
+
+def about(request):
+  return HttpResponse(request, 'about')
