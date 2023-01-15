431d9554da959c796a4a967cfad73cfed923b7ab
RaimundoJSoares <raianimes1000@gmail.com>
2023-01-15 11:54:15 -0400

    add movies

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from movie import views as movieViews
+from django.conf.urls.static import static
+from  django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,7 @@ urlpatterns = [
     path('about/', movieViews.about, name='about'),
     path('signup/', movieViews.signup, name='signup'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+document_root = settings.MEDIA_ROOT
+    )
